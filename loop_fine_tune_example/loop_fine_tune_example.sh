2d0574eaca1f4706631319b88499b5373b3c5637
aaronbanse <acbanse@icloud.com>
2024-09-10 13:12:47 -0700

    changed to save every epoch

--- loop_fine_tune_example/loop_fine_tune_example.sh
+++ loop_fine_tune_example/loop_fine_tune_example.sh
@@ -5,11 +5,12 @@
 # are specified for design, instead of one or more chains
 
 python ../training/training.py \
-    --path_for_outputs "output/" \
-    --path_for_training_data "loop_fine_tune_data/" \
+    --path_for_outputs "output/version_2" \
+    --path_for_training_data "training_data_v2/" \
     --previous_checkpoint "../vanilla_model_weights/v_48_020.pt" \
     --mixed_precision 0 \
     --num_examples_per_epoch 48 \
     --num_epochs 20 \
     --batch_size 1000 \
-    --use_custom_design_mask 1
+    --use_custom_design_mask 1 \
+    --save_model_every_n_epochs 1
